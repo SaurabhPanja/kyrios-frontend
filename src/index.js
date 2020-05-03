048dd9576cf36699641dfc43fbb78635b996dcad
Saurabh Panja <saurabh@market-pulse.in>
2020-05-03 18:36:00 +0530

    navbar added, bootstrap installed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 
 // Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarRender from './navbarRender';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<NavbarRender />, document.getElementById('navbar'));
+// ReactDOM.render()
