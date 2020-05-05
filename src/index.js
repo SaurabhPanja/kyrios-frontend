0a04dfa19a3c3a502547f564a9d459ab7838c68c
Saurabh Panja <saurabh@market-pulse.in>
2020-05-06 01:25:46 +0530

    added sell button, styled it

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './App.css'
 // import App from './App';
 
 // Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarRender from './navbarRender';
 import CardRender from './cardRender'
+import SellButton from './sellButton'
 
 ReactDOM.render(<NavbarRender />, document.getElementById('navbar-render'));
 ReactDOM.render(<CardRender></CardRender>, document.getElementById('card-render'))
+ReactDOM.render(<SellButton></SellButton>, document.getElementById('sell-button'))
---
+++ src/sellButton.js
@@ -0,0 +1,13 @@
+import React from "react"
+import {Button} from "react-bootstrap"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCamera } from '@fortawesome/free-solid-svg-icons'
+
+const sellButton = () =>(
+    <Button variant="primary" className="font-weight-bold">
+        <FontAwesomeIcon className="mr-2" icon={faCamera}></FontAwesomeIcon>
+        <span>SELL</span> 
+    </Button>
+)
+
+export default sellButton
